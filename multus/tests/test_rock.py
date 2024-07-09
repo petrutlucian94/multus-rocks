464411570eeb1e9861011c94f352288f1bfbab5a
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2024-07-09 06:16:02 +0000

    Add github actions
    We're defining github actions that:
    
    * build the rock and ensure that it contains the expected files
    * scan for CVEs
    * push the images (will require access)

---
+++ multus/tests/test_rock.py
@@ -0,0 +1,44 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+#
+
+import random
+import pytest
+import string
+import subprocess
+
+from charmed_kubeflow_chisme.rock import CheckRock
+
+
+def check_image_paths(image, paths):
+    for path in paths:
+        subprocess.run(
+            [
+                "docker", "run", image,
+                "ls", "-l", path
+            ],
+            check=True,
+        )
+
+
+def test_rock():
+    """Test rock."""
+    check_rock = CheckRock("rockcraft.yaml")
+    rock_image = check_rock.get_name()
+    rock_version = check_rock.get_version()
+    LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
+
+    # check rock filesystem
+    check_image_paths(
+        LOCAL_ROCK_IMAGE,
+        [
+            "/install_multus",
+            "/thin_entrypoint",
+            "/usr/src/multus-cni/LICENSE",
+            "/usr/src/multus-cni/bin/install_multus",
+            "/usr/src/multus-cni/bin/thin_entrypoint",
+            "/usr/src/multus-cni/bin/multus",
+            "/usr/src/multus-cni/bin/multus-daemon",
+            "/usr/src/multus-cni/bin/multus-shim",
+        ])
